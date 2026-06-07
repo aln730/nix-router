b9771cd315ff548ff3b8144962aa4c555ec84b81
aln730 <arnavgawas707@gmail.com>
2026-06-07 00:28:20 -0400

    basics of networking. I need to harden some stuff

---
+++ modules/networking.nix
@@ -0,0 +1,33 @@
+{ config, lib, pkgs, ... };
+
+{
+  networking.hostName = "quartz";
+  networking.hostId = "625673fb4a24af96";
+  time.timeZone = "America/New_York";
+
+  networking.useDHCP = false;
+  networking.interfaces.enp1s0.useDHCP = true;
+  networking.interfaces.enp2s0 = {
+    useDHCP = false;
+    ipv4.addresses = [{
+      address = "192.168.1.1";
+      prefixLength = 24;
+    }];
+  };
+
+  boot.kernel.sysctl = {
+    "net.ipv4.ip_forward" = 1;
+    "net.ipv6.conf.all.forwarding" = 1;
+    "net.ipv4.conf.all.rp_filter" = 1;
+    "net.ipv4.conf.default.rp_fitler" = 1;
+    "net.ipv4.conf.all.accept_redirects" = 0;
+    "net.ipv4.conf.all.send_redirects"   = 0;
+  };
+
+  environment.systemPackages = with pkgs; [
+    ethtool
+    tcpdump
+    iproute2
+    nftables
+  ];
+}
