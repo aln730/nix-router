325715e58a8f279e12c79a45753b07ec57cdec79
aln730 <arnavgawas707@gmail.com>
2026-06-07 00:14:01 -0400

    init

---
+++ flake.nix
@@ -0,0 +1,18 @@
+{
+  description = "quartz";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs, ... }@inputs: {
+    nixosConfigurations.quartz = nixpkgs.lib.nixosSystem {
+      system = "x86_64-linux";
+      specialArgs = { inherit inputs; };
+      modules = [
+        ./hardware-configuration.nix
+        ./modules/networking.nix
+      ];
+    };
+  };
+}
